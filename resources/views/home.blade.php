d3b3b60b4dc4c974fcea591bc1db4480456862a7
tommyrharper <thomasrobertharper@gmail.com>
2020-08-26 19:25:22 +0100

    Fix view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,13 @@
 <div class="container">
     <div class="row">
         <div class="col3 p-5">
-            <img src="https://instagram.flhr3-2.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.flhr3-2.fna.fbcdn.net&_nc_ohc=M-I68HMHqW0AX_Sv6Nf&oh=94a2d6432756c51980c703ae5cb36a91&oe=5F6E5B67" alt="" class="rounded-circle">
+            <img src="https://instagram.flhr3-2.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.flhr3-2.fna.fbcdn.net&_nc_ohc=M-I68HMHqW0AX_Sv6Nf&oh=94a2d6432756c51980c703ae5cb36a91&oe=5F6E5B67" alt="" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ $user->username }}</h1><div>
+            <div>
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> Posts</div>
                 <div class="pr-5"><strong>23k</strong> Followers</div>
@@ -19,7 +22,7 @@
         </div>
     </div>
 
-    <div class="row pt-4">
+    <div class="row pt-5">
         <div class="col-4">
             <img src="https://instagram.flhr3-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/118287795_1049613382163863_2826749017340034474_n.jpg?_nc_ht=instagram.flhr3-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Uj4DVhg78ScAX9ZSGMz&oh=7ffca165b1228773c640ad54e8289238&oe=5F6E69A0" alt="" class="w-100">
         </div>
