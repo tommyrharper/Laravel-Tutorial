55871629526c2b3f94bc42a604eb477bc74426de
tommyrharper <thomasrobertharper@gmail.com>
2020-08-26 20:08:43 +0100

    Add validation in the PostsController store() method

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -13,6 +13,11 @@ class PostsController extends Controller
 
     public function store()
     {
+        $data = request()->validate([
+            'caption' => 'required',
+            'image' => 'required',
+        ]);
+
         dd(request()->all());
     }
 }
