44e2b7b81aad63d82609f241a60e0dcb48a70736
tommyrharper <thomasrobertharper@gmail.com>
2020-08-26 19:08:59 +0100

    Add hasMany function for user -> posts

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Profile::class);
     }
+
+    public function user()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
