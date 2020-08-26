ef481e71c3604068a524f7f502db329557487399
tommyrharper <thomasrobertharper@gmail.com>
2020-08-26 20:24:02 +0100

    Allow certain values through the Posts model

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = ['caption', 'user_id', 'created_at', 'image'];
+    protected $gaurded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
