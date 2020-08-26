86f03417c8cd4fec7b74416e8a9d6059752da080
tommyrharper <thomasrobertharper@gmail.com>
2020-08-26 18:09:56 +0100

    Remove old comment

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,7 +10,6 @@ class ProfilesController extends Controller
     public function index($user)
     {
         $user = User::find($user);
-        // dd($user);
         return view('home', [
             'user' => $user,
         ]);
