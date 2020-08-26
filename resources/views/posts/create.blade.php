85d7911a947ef4faf7a4c8505aafc7988cb134b4
tommyrharper <thomasrobertharper@gmail.com>
2020-08-26 19:52:54 +0100

    Update create post view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,48 @@
 
 @section('content')
 <div class="container">
+  <form action="">
+    <div class="row">
+      <div class="col-8 offset-2">
 
+      <div class="row">
+        <h1>Add New Post</h1>
+      </div>
+
+        <div class="form-group row">
+            <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+            
+            <input id="caption" 
+                  type="text" 
+                  class="form-control @error('caption') is-invalid @enderror" 
+                  caption="caption" value="{{ old('caption') }}"  
+                  autocomplete="caption" autofocus>
+
+              @if ($errors->has('caption'))
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $erros->first('caption') }}</strong>
+                  </span>
+              @endif
+        </div>
+
+        <div class="row">
+          <label for="image" class="col-md-4 col-form-label">Post Image</label>
+
+          <input type="file" class="form-control-file" id="image" name="image">
+
+          @if ($errors->has('image'))
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $erros->first('image') }}</strong>
+              </span>
+          @endif
+        </div>
+
+        <div class="row pt-4">
+          <button class="btn btn-primary">Add New Post</button>
+        </div>
+
+      </div>
+    </div>
+  </form>
 </div>
 @endsection
