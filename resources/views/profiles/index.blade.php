e75adb88dd715e6ca08a2a4113de766407c5e017
tommyrharper <thomasrobertharper@gmail.com>
2020-09-12 18:08:20 +0100

    Render images dynamically

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,7 +9,7 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="#">Add New Post</a>
+                <a href="/p/create">Add New Post</a>
             </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> Posts</div>
@@ -23,15 +23,11 @@
     </div>
 
     <div class="row pt-5">
+        @foreach($user->posts as $post)
         <div class="col-4">
-            <img src="https://instagram.flhr3-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/118287795_1049613382163863_2826749017340034474_n.jpg?_nc_ht=instagram.flhr3-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Uj4DVhg78ScAX9ZSGMz&oh=7ffca165b1228773c640ad54e8289238&oe=5F6E69A0" alt="" class="w-100">
+            <img src="/storage/{{ $post->image }}" alt="" class="w-100">
         </div>
-        <div class="col-4">
-            <img src="https://instagram.flhr3-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/118287795_1049613382163863_2826749017340034474_n.jpg?_nc_ht=instagram.flhr3-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Uj4DVhg78ScAX9ZSGMz&oh=7ffca165b1228773c640ad54e8289238&oe=5F6E69A0" alt="" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="https://instagram.flhr3-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/118287795_1049613382163863_2826749017340034474_n.jpg?_nc_ht=instagram.flhr3-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Uj4DVhg78ScAX9ZSGMz&oh=7ffca165b1228773c640ad54e8289238&oe=5F6E69A0" alt="" class="w-100">
-        </div>
+        @endforeach
     </div>
 </div>
 @endsection
