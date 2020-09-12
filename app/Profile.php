6ef5bf2f206e886433f7f1f69472774202425943
tommyrharper <thomasrobertharper@gmail.com>
2020-09-12 20:34:39 +0100

    Add profileImage method to Profile model

--- app/Profile.php
+++ app/Profile.php
@@ -9,6 +9,11 @@ class Profile extends Model
     protected $fillable = ['title', 'description', 'url', 'image'];
     protected $gaureded = [];
 
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/0b2rQ4jUnVrwt8Oa2b5tSBEDALH0WsvXXNzW06z6.png';
+        return '/storage/' . $imagePath;
+    }
     public function user()
     {
         return $this->belongsTo(User::class);
