b4afad79eefd04333ff9a1f2334da47a250f0b99
tommyrharper <thomasrobertharper@gmail.com>
2020-09-13 00:23:15 +0100

    Add route for FollowsController

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::post('follow/{user}', 'FollowsController@Store');
+
 Route::get('/p/create', 'PostsController@create');
 Route::get('/p/{post}', 'PostsController@show');
 Route::post('/p', 'PostsController@store');
