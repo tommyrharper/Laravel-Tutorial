5dd9e82c3ea14465399514e072a164348b3d44c0
tommyrharper <thomasrobertharper@gmail.com>
2020-09-12 19:03:07 +0100

    Allow updates on Profile model

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $fillable = ['title', 'description', 'url', 'image'];
+    protected $gaureded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
