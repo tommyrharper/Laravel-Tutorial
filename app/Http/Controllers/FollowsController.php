5a6b9441e5e0885c8bccda5fa0b2d076a35b8e4d
tommyrharper <thomasrobertharper@gmail.com>
2020-09-20 19:49:26 +0100

    Import User into FollowsController.php

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class FollowsController extends Controller
 {
-    public function store()
+    public function store(User $user)
     {
-        
+        return $user->username;
     }
 }
