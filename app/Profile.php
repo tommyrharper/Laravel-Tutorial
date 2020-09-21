9f0fbed43ca6d365052157adc3a9a862aedbf6ae
tommyrharper <thomasrobertharper@gmail.com>
2020-09-21 20:34:25 +0100

    Add followers function to Profile.php

--- app/Profile.php
+++ app/Profile.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
@@ -14,6 +15,12 @@ class Profile extends Model
         $imagePath = ($this->image) ? $this->image : 'profile/0b2rQ4jUnVrwt8Oa2b5tSBEDALH0WsvXXNzW06z6.png';
         return '/storage/' . $imagePath;
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
