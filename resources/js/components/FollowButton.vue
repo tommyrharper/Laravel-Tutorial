e62fd18763cd7f09d3a83fb421ee4be435b39713
tommyrharper <thomasrobertharper@gmail.com>
2020-09-21 20:43:12 +0100

    Catch Follow erros in FollowButton

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -25,6 +25,11 @@
                         this.status = ! this.status;
 
                         console.log(response.data);
+                    })
+                    .catch(errors => {
+                        if (errors.response.status === 401) {
+                            window.location = '/login';
+                        }
                     });
             }
         },
