554d6a90a4da867a241251003ade483b2b39b626
tommyrharper <thomasrobertharper@gmail.com>
2020-09-21 20:40:24 +0100

    Block unauthorised follows in FollowsController using $this->middleware('auth')

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 
 class FollowsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function store(User $user)
     {
         return auth()->user()->following()->toggle($user->profile);
